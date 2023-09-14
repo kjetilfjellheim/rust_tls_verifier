dcdac46e3f6c747a28be9f8368d91c1ae80b6853
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2023-09-14 23:07:16 +0200

    refactor: Reformatting code
    Refactored code to make easier to read. No changes to functionality.
    
    Breaking changes: None
    
    Resolves: None

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::io::Read;
+use reqwest::blocking::Client;
+use reqwest::{blocking::ClientBuilder, Certificate, Identity, Proxy};
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::fs::File;
-use reqwest::{Identity, Proxy, blocking::ClientBuilder, Certificate};
-use serde::{Deserialize, Serialize};
+use std::io::Read;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
-use reqwest::blocking::Client;
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Log<'a> {
-    logdata: &'a str
+    logdata: &'a str,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -26,7 +26,7 @@ struct Input<'a> {
     check_hostname: bool,
     use_inbuilt_root_certs: bool,
     https_only: bool,
-    use_tls_sni: bool
+    use_tls_sni: bool,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -37,38 +37,30 @@ struct ApplicationError {
 
 impl ApplicationError {
     pub fn new(message: String) -> ApplicationError {
-        ApplicationError {       
-            message: message
-        }
+        ApplicationError { message: message }
     }
 }
 
-fn read_file<'a,'b>(filename: &'a str) -> Result<Vec<u8>, ApplicationError> {
+fn read_file<'a, 'b>(filename: &'a str) -> Result<Vec<u8>, ApplicationError> {
     let file = File::open(filename);
     let mut file: File = match file {
-        Ok(file) => { file },
-        Err(error) => {
-            return Err(ApplicationError::new(String::from(error.to_string())))
-        }
+        Ok(file) => file,
+        Err(error) => return Err(ApplicationError::new(String::from(error.to_string()))),
     };
     let mut buf = Vec::new();
     let read = file.read_to_end(&mut buf);
     match read {
-        Ok(_size) => { Ok(buf) },
-        Err(error) => {
-            Err(ApplicationError::new(String::from(error.to_string())))
-        }
+        Ok(_size) => Ok(buf),
+        Err(error) => Err(ApplicationError::new(String::from(error.to_string()))),
     }
 }
 
-fn get_certificate<'a, 'b >(certificate_path: &'a str) -> Result<Certificate, ApplicationError> {
+fn get_certificate<'a, 'b>(certificate_path: &'a str) -> Result<Certificate, ApplicationError> {
     let buffer = read_file(certificate_path)?;
     let certificate = reqwest::Certificate::from_pem(&buffer);
     match certificate {
-        Ok(certificate) => { return Ok(certificate)  },
-        Err(error) => { 
-            Err(ApplicationError::new(String::from(error.to_string()))) 
-        }
+        Ok(certificate) => return Ok(certificate),
+        Err(error) => Err(ApplicationError::new(String::from(error.to_string()))),
     }
 }
 
@@ -80,7 +72,16 @@ fn get_clientbuilder() -> ClientBuilder {
     client
 }
 
-fn get_client<'a>(public_certificate_path: &str, keystore_path: &str, keystore_password: &str, proxy_url: Option<&str>, check_hostname: bool, use_inbuilt_root_certs: bool, use_tls_sni: bool, use_https_only: bool) -> Result<Client, ApplicationError> {
+fn get_client<'a>(
+    public_certificate_path: &str,
+    keystore_path: &str,
+    keystore_password: &str,
+    proxy_url: Option<&str>,
+    check_hostname: bool,
+    use_inbuilt_root_certs: bool,
+    use_tls_sni: bool,
+    use_https_only: bool,
+) -> Result<Client, ApplicationError> {
     let certificate = get_certificate(public_certificate_path)?;
 
     let identity = get_identity(keystore_path, keystore_password)?;
@@ -89,39 +90,37 @@ fn get_client<'a>(public_certificate_path: &str, keystore_path: &str, keystore_p
         .tls_built_in_root_certs(use_inbuilt_root_certs)
         .add_root_certificate(certificate)
         .identity(identity)
-        .https_only(use_https_only)     
+        .https_only(use_https_only)
         .connection_verbose(true)
         .tls_sni(use_tls_sni)
         .http1_only()
         .danger_accept_invalid_hostnames(!check_hostname);
 
-    let clientbuilder: ClientBuilder = match proxy_url  {
+    let clientbuilder: ClientBuilder = match proxy_url {
         Some(proxy_url) => {
             let proxy = Proxy::all(proxy_url).expect("Failure setting proxy url");
             clientbuilder.proxy(proxy)
-        },
-        None => { clientbuilder },
+        }
+        None => clientbuilder,
     };
 
     let client = clientbuilder.build();
     match client {
-        Ok(client) => { Ok(client) },
-        Err(error) => { 
-            Err(ApplicationError::new(String::from(error.to_string())))
-        }
+        Ok(client) => Ok(client),
+        Err(error) => Err(ApplicationError::new(String::from(error.to_string()))),
     }
-
 }
 
 #[cfg(feature = "native-tls")]
-fn get_identity<'a,'b >(keystore_path: &'a str, keystore_password: &'a str) -> Result<Identity, ApplicationError> {
+fn get_identity<'a, 'b>(
+    keystore_path: &'a str,
+    keystore_password: &'a str,
+) -> Result<Identity, ApplicationError> {
     let buffer = read_file(keystore_path)?;
     let identity = reqwest::Identity::from_pkcs12_der(&buffer, keystore_password);
     match identity {
-        Ok(identity) => { return Ok(identity)  },
-        Err(error) => { 
-            Err(ApplicationError::new(String::from(error.to_string()))) 
-        }
+        Ok(identity) => return Ok(identity),
+        Err(error) => Err(ApplicationError::new(String::from(error.to_string()))),
     }
 }
 
@@ -136,23 +135,30 @@ fn get_identity(keystore: &str, keystore_password: &str) -> Identity {
 }
 
 #[tauri::command(rename_all(deserialize = "snake_case", serialize = "camelCase"))]
-fn do_request<'a, 'b>(input : Input) -> Result<Log, ApplicationError> {
-    let client = get_client(input.public_certificate_path, input.keystore_path, input.keystore_password, input.proxy_url, input.check_hostname, input.use_inbuilt_root_certs, input.https_only, input.use_tls_sni)?;
-    let response = client.get(input.url).send();    
+fn do_request<'a, 'b>(input: Input) -> Result<Log, ApplicationError> {
+    let client = get_client(
+        input.public_certificate_path,
+        input.keystore_path,
+        input.keystore_password,
+        input.proxy_url,
+        input.check_hostname,
+        input.use_inbuilt_root_certs,
+        input.https_only,
+        input.use_tls_sni,
+    )?;
+    let response = client.get(input.url).send();
     match response {
-        Ok(_) => { Ok(Log { logdata: "Success" }) },
-        Err(error) => { 
-            Err(ApplicationError::new(String::from(error.to_string())))
-        }
+        Ok(_) => Ok(Log { logdata: "Success" }),
+        Err(error) => Err(ApplicationError::new(String::from(error.to_string()))),
     }
 }
 
 fn main() {
     let subscriber = FmtSubscriber::builder()
-    .with_max_level(Level::TRACE)
-    .finish();
+        .with_max_level(Level::TRACE)
+        .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-    
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![do_request])
         .run(tauri::generate_context!())
--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ async function do_request() {
   input.https_only = document.querySelector("#idHttpsOnly").checked;
   input.use_tls_sni = document.querySelector("#idUseTlsSni").checked;
 
-  await invoke("do_request", {"input": input})
+  await invoke("do_request", { "input": input })
     .then((response) => {
       const val = response instanceof ArrayBuffer ? new TextDecoder().decode(response) : response
       document.querySelector("#logdata").innerText = response.logdata;
